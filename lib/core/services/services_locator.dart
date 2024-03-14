46278b97063249caf502b3772070febb9c8d543e
ebrahim esmail <147981042+ebrahimesmail11@users.noreply.github.com>
2024-03-14 13:36:10 +0200

    Add newest books cubit and state, and services locator setup
    - Added newest books cubit and state classes for managing newest books data
    - Set up services locator for dependency injection using GetIt

---
+++ lib/core/services/services_locator.dart
@@ -0,0 +1,10 @@
+import 'package:bookly/Features/home/data/repos/home_repo_imp.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+final getIt=GetIt.instance;
+void setUpServicesLocator(){
+  getIt.registerSingleton<ApiService>(ApiService(Dio(),),);
+  getIt.registerSingleton<HomeRepoImp>(HomeRepoImp(getIt.get<ApiService>()),);
+}
