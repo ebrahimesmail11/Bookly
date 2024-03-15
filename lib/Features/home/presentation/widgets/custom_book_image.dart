c3f8aeef2de7da43f4db84810e3408203a671a5b
ebrahim esmail <147981042+ebrahimesmail11@users.noreply.github.com>
2024-03-15 14:35:24 +0200

    Add cached_network_image package and update dependencies
    - Import cached_network_image package for network image caching
    - Update pubspec.yaml to include cached_network_image version 3.3.1
    - Update pubspec.lock with new cached_network_image dependencies and versions

--- lib/Features/home/presentation/widgets/custom_book_image.dart
+++ lib/Features/home/presentation/widgets/custom_book_image.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class CustomBookImage extends StatelessWidget {
@@ -5,19 +6,15 @@ class CustomBookImage extends StatelessWidget {
 final String imageUrl;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.4 / 4,
-      child: Container(
-        decoration: BoxDecoration(
-          image:  DecorationImage(
-              image: NetworkImage(
-                imageUrl,
-              ),
-              fit: BoxFit.fill),
-          borderRadius: BorderRadius.circular(
-            18,
-          ),
-        ),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(18),
+      child: AspectRatio(
+        aspectRatio: 2.4 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          imageUrl: imageUrl,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+       ),
       ),
     );
   }
